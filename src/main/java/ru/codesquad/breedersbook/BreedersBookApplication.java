6bd88aa97128bccc837d67c19bd7489b431ac0ed
Danylena <eleorex@yandex.ru>
2023-09-13 19:21:02 +0300

    I was born on programmer's day

---
+++ src/main/java/ru/codesquad/breedersbook/BreedersBookApplication.java
@@ -0,0 +1,12 @@
+package ru.codesquad.breedersbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreedersBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreedersBookApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/codesquad/breedersbook/BreedersbookApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.codesquad.breedersbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreedersbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
