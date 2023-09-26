e836efe281dba36220aa3db0774864bb35cd7a20
Denis <leonenko.denis.1@yandex.ru>
2023-09-26 13:53:48 +0300

    00.add-userMapper/01.03-fix: refactor UserMapper.java, return interface for mapper

--- bb-service/src/main/java/ru/codesquad/user/dto/UserMapper.java
+++ bb-service/src/main/java/ru/codesquad/user/dto/UserMapper.java
@@ -2,7 +2,6 @@ package ru.codesquad.user.dto;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.beans.factory.annotation.Autowired;
 import ru.codesquad.kennel.dto.KennelMapper;
 import ru.codesquad.user.User;
 import ru.codesquad.userinfo.dto.UserInfoDto;
@@ -12,23 +11,16 @@ import ru.codesquad.util.enums.Gender;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Mapper(componentModel = "spring",
+@Mapper(uses = {UserInfoMapper.class, KennelMapper.class},
+        componentModel = "spring",
         imports = {LocalDateTime.class, UUID.class, Gender.class, UserInfoDto.class},
         disableSubMappingMethodsGeneration = true)
-public abstract class UserMapper {
-
-    @Autowired
-    protected UserInfoMapper userInfoMapper;
-    @Autowired
-    protected KennelMapper kennelMapper;
-
-    @Mapping(target = "kennel", expression = "java(kennelMapper.returnKennelDto(user.getKennel()))")
-    @Mapping(target = "userInfo", expression = "java(userInfoMapper.returnUserInfoDto(user.getUserInfo()))")
-    public abstract UserDto returnUserDto(User user);
+public interface UserMapper {
+    UserDto returnUserDto(User user);
 
     @Mapping(target = "gender", expression = "java(Gender.getGenderValue(userNewDto.getGender()))")
     @Mapping(target = "created", expression = "java(LocalDateTime.now())")
-    public abstract User returnUser(UserNewDto userNewDto);
+    User returnUser(UserNewDto userNewDto);
 
-    public abstract UserShortDto returnUserShortDto(User user);
+    UserShortDto returnUserShortDto(User user);
 }
